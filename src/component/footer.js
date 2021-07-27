63ba7afc4cc701d0058ac82ebb57a308c037d530
saraalshater <gleave.alshater@gmail.com>
2021-07-27 09:02:32 +0300

    almost done

---
+++ src/component/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Footer extends React.Component{
+    render(){
+        return(
+        <div>
+        <p> Sara alshater</p>
+        </div>
+        );
+    }
+}
+
+export default Footer;
